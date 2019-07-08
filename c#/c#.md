dd45747b70c95cd8221b4c345bf7ba5ab3f75480
tangjialiang <phx108@sina.com>
2019-07-08 23:34:33 +0800

    new

--- c#/c#.md
+++ c#/c#.md
@@ -10,3 +10,9 @@
     + int num = (int) ht[key] // (k, v) is (obj, obj)
 - 数组的长度获取：
     + int len = int[3]{1, 2, 3}.Length
+
+### pro_3
+- 貌似在c#中，没有范型
+- 在C#中，函数都是大写首字母
+    + "".Equals()
+- 在leetcode上，避免使用类的static property, 这种成员变量会被上次执行的结果影响.
--- c#/travel_01/Solution_003.cs
+++ c#/travel_01/Solution_003.cs
@@ -1,5 +1,41 @@
 public class Solution {
+    static int maxWL = 0, maxWR = 0;
+    
     public int LengthOfLongestSubstring(string s) {
+        if (s.Equals("")) {
+            return 0; 
+        }
         
+        maxWL = 0; 
+        maxWR = 0;
+        int wL = 0, wR = 0; 
+        Hashtable ht = new Hashtable(); // <char, idx>
+        
+        int lastI = 0; 
+        for (int i=0; i<s.Length; i++) {
+            char c = s[i]; 
+            int idx = i; 
+            if (ht.Contains(c)) {
+                int newIdx = (int)ht[c] + 1; 
+                while (lastI < newIdx) {
+                    ht.Remove(s[lastI]); 
+                    lastI++; 
+                }
+                ht.Add(c, idx); 
+            } else {
+                // not in records. 
+                ht.Add(c, idx); 
+            }
+            updateMaxW(lastI, i); 
+        }
+        
+        return maxWR - maxWL + 1; 
+    }
+    
+    private void updateMaxW(int wL, int wR) {
+        if (wR - wL > maxWR - maxWL) {
+            maxWL = wL; 
+            maxWR = wR; 
+        }
     }
 }
